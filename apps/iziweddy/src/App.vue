db912940f86924f7b415200eeaa3f4702f25937e
Libor Fridrich <l.fridrich@esigno.com>
2026-09-11 11:22:47 +0200

    Remade

--- apps/iziweddy/src/App.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router';
-</script>
-
-<template>
-  <a class="skip-link" href="#obsah">Přeskočit na obsah</a>
-  <RouterView />
-</template>
--- apps/iziweddy/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createPinia } from 'pinia';
-import { createApp } from 'vue';
-import App from './App.vue';
-import { router } from './router';
-import './style.css';
-
-// Pinia musí být nainstalovaná dřív než router – jeho beforeEach sahá na store.
-createApp(App).use(createPinia()).use(router).mount('#app');
